85f3fa610d528d83ec28160d40dbd35127d8ba27
codal-vvariyani <vvariyani@codal.com>
2019-01-09 19:29:11 +0530

    commited

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
+
+export interface postResponse {
+  body?: string;
+  id?: Number;
+  title?: string;
+  userId?: Number;
+}
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-  posts: any[];
+  posts: postResponse[];
+  
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
   constructor(private http: HttpClient) {
-    this.http.get('http:/192.168.1.158')
+    this.http.get<postResponse[]>(this.url)
     .subscribe(response => {
-      console.log(response);
-    });
+      this.posts = response;
+      });
    }
+
+   createPost(input: HTMLInputElement) {
+     let postTitle = {title: input.value}; 
+    this.http.post(this.url, JSON.stringify(postTitle)).subscribe(response => {  
+      console.log(response); 
+    });
+  } 
 
   ngOnInit() {
   }
